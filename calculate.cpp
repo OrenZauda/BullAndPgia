ebfa3f179a6be7b213a5e2504957d113681eb7f7
Avichay Nega <44750349+avichaynega@users.noreply.github.com>
2019-04-30 21:42:01 +0300

    Update calculate.cpp

--- calculate.cpp
+++ calculate.cpp
@@ -7,6 +7,22 @@ using std::string;
 
 namespace bullpgia{
 string calculateBullAndPgia(string choice, string guess){
-    return " ";
+     int bull = 0, pgia = 0;
+    for (int i = 0; i < choice.length(); i++)
+    {
+        if (choice[i] == guess[i])
+        {
+            bull++;
+            choice[i] = 't';
+            guess[i] = 's';
+        }
+    }
+    for (int i = 0; i < guess.length(); i++)
+    {
+        if (choice.find(guess[i]) != string::npos)
+            pgia++;
+    }
+    string bullAndPgia = to_string(bull) + "," + to_string(pgia);
+    return bullAndPgia;
 }
 };
