d5cb7c771a187bcdcbc28d2ea5253d94a42690e2
OrenZauda <45006271+OrenZauda@users.noreply.github.com>
2019-05-01 13:19:12 +0300

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -32,6 +32,12 @@ int main() {
 		testcase.setname("Calculate bull and pgia")
 		.CHECK_OUTPUT(calculateBullAndPgia("1234","1234"), "4,0")      // 4 bull, 0 pgia
 		.CHECK_OUTPUT(calculateBullAndPgia("1234","4321"), "0,4")      // 0 bull, 4 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("1234","1000"), "1,0")
+		.CHECK_OUTPUT(calculateBullAndPgia("1234","1111"), "1,0")
+		.CHECK_OUTPUT(calculateBullAndPgia("1234","1984"), "2,0")
+		.CHECK_OUTPUT(calculateBullAndPgia("1234","29843"), "0,2")
+		.CHECK_OUTPUT(calculateBullAndPgia("1234","1984"), "2,0")
+		.CHECK_OUTPUT(calculateBullAndPgia("9999","1984"), "1,0")
 		;
 
 		testcase.setname("Play with dummy choosers and guessers")
@@ -39,19 +45,16 @@ int main() {
 		.CHECK_EQUAL(play(c1234, g9999, 4, 100), 101)    // guesser loses by running out of turns 
 		.CHECK_EQUAL(play(c1234, g12345, 4, 100), 101)   // guesser loses technically by making an illegal guess (too long).
 		.CHECK_EQUAL(play(c12345, g1234, 4, 100), 0)     // chooser loses technically by choosing an illegal number (too long).
-		.CHECK_EQUAL(play(c12345, g12345, 4, 100), 1)
-		.CHECK_EQUAL(play(c9999, g12345, 4, 100), 0)
-		.CHECK_EQUAL(play(c9999, g12345, 4, 100), 0)
-		.CHECK_EQUAL(play(c9999, g12345, 4, 100), 0)
-		.CHECK_EQUAL(play(c9999, g12345, 4, 100), 0)
-		.CHECK_EQUAL(play(c1234, g1234, 4, 100), 1)
-		.CHECK_EQUAL(play(c9999, g1234, 4, 100), 101)
-		.CHECK_OUTPUT(calculateBullAndPgia("1234","1000"), "1,0")
-		.CHECK_OUTPUT(calculateBullAndPgia("1234","1111"), "1,0")
-		.CHECK_OUTPUT(calculateBullAndPgia("1234","1984"), "2,0")
-		.CHECK_OUTPUT(calculateBullAndPgia("1234","29843"), "0,2")
-		.CHECK_OUTPUT(calculateBullAndPgia("1234","1984"), "2,0")
-		.CHECK_OUTPUT(calculateBullAndPgia("9999","1984"), "1,0")
+		.CHECK_EQUAL(play(c12345, g1234, 4, 100), 0)
+		.CHECK_EQUAL(play(c123335, g1234, 4, 100), 0)
+		.CHECK_EQUAL(play(c123245, g1234, 4, 100), 0)
+		.CHECK_EQUAL(play(c120345, g1234, 4, 100), 0)
+		.CHECK_EQUAL(play(c123455, g1234, 4, 100), 0)
+		.CHECK_EQUAL(play(c123, g123, 3, 100), 1)
+		.CHECK_EQUAL(play(c0876, g1234, 4, 100), 101)
+		.CHECK_EQUAL(play(c0954, g1234, 4, 100), 101)
+		.CHECK_EQUAL(play(c0321, g1234, 4, 100), 101)
+		.CHECK_EQUAL(play(c8905, g1234, 4, 100), 101)
 		;
 
 		testcase.setname("Play with smart guesser");
