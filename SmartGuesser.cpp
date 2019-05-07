6430288eec42e8e44a6ac6fada7160fd57e3b9f9
Avichay Nega <44750349+avichaynega@users.noreply.github.com>
2019-05-07 23:26:14 +0300

    Update SmartGuesser.cpp

--- SmartGuesser.cpp
+++ SmartGuesser.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 using std::string;
 
-
 namespace bullpgia
 {
+ //constructors
 SmartGuesser::SmartGuesser(const string &guess)
 {
     _guess = guess;
@@ -18,14 +18,17 @@ SmartGuesser::SmartGuesser()
     _length = _guess.length();
 }
 
-// basic finction
+////////////////////////////// basic finction///////////////////////////////////////
+
+//this method return the next guss in the game
 string SmartGuesser::guess()
 {
-    
+
     long teste = -1;
-    
+// when start= true, we are in the first time
     if (start)
     {
+         // first time the guess is in the form "00..11"
         _guess = "";
         for (int i = 0; i < p; i++)
         {
@@ -38,62 +41,52 @@ string SmartGuesser::guess()
         return _guess;
     }
 
-    
     long newTeste;
-  
-     newTeste = firstActive();
-    
-     
+     //insert the new guess to the field _guess
+
+    newTeste = firstActive();
+
     _guess = to_string(newTeste);
     complete(_guess);
     return _guess;
-   
 }
-void SmartGuesser::complete(string guess)
-{
-    string zero = "";
-    for(int i=guess.length();i<p;i++){
-        zero="0"+zero;
-    }
-    _guess = zero + _guess;
 
-}
+//this method start new game
 void SmartGuesser::startNewGame(uint length)
 {
-   
+
     start = true;
     _length = length;
     p = length;
-    
 
     combinations = pow(m, p);
+
     
+    // array of booleans to exclude in next search
+    active = new bool[combinations]; 
     // Initialization of possibilities
-     active = new bool[combinations]; // array of booleans to exclude in next search
     b = new long[combinations];
-    
+
     initArrayFromZero();
-    
-    for (int i = 0; i < combinations; i++)
-    {
-        active[i] = 1;
-    }
-    
-   
+
 }
 
+// this method recived the  number of cows and bulls and
+// calculate the next steps.
 void SmartGuesser::learn(string reply)
 {
-
+// exclude the bad guesses, according the reply
     int responsePegs = ((int)reply[0] - 48) * 10;
     responsePegs += (int)reply[2] - 48;
     excludeNumbersWithSameCode(stol(_guess), responsePegs);
 }
 
-// other functions
+//////////////////////////////// other functions///////////////////////////////////////////
 
+// this method return the lowes values between two values
 long SmartGuesser::min(long a, long b) { return a < b ? a : b; }
 
+// this method return the number of cows and bulls between two guesses
 int SmartGuesser::testCode(long guess, long code)
 {
     /// Test code for # of black and white pegs
@@ -142,13 +135,14 @@ int SmartGuesser::testCode(long guess, long code)
         if (occurring[i] != 0)
             whites += occurring[i];
     }
-    
+
     return blacks * 10 + whites;
 }
 
+// this method return long number which is some possible gueess
 long SmartGuesser::composeNum(long j, int m)
 {
-    
+
     /// Translate number j from decimal into a numeral system with base m
     long r = j % m;
     long b = j / m;
@@ -162,22 +156,22 @@ long SmartGuesser::composeNum(long j, int m)
     return r;
 }
 
-
+// this method collect all the possible guess to a array
 void SmartGuesser::initArrayFromZero()
 {
-    
-    /// Recursively initialize array [{0..m},{1..m},{2..m} ... {n..m}]
+
+   
     for (long i = 0; i < combinations; i++)
     {
         b[i] = composeNum(i, m);
+         active[i] = 1;
     }
 }
 
-
-
+// this method cut all the bad guesses
 void SmartGuesser::excludeNumbersWithSameCode(long number, long code)
 {
-    
+
     /// For each number with not the same # of blacks and whites set "not active"
     for (long i = 0; i < combinations; ++i)
     {
@@ -191,7 +185,7 @@ void SmartGuesser::excludeNumbersWithSameCode(long number, long code)
 long SmartGuesser::firstActive()
 {
     long minimumNumber = -1;
-    for (long i = 0; i < combinations ; ++i)
+    for (long i = 0; i < combinations; ++i)
     {
         if (active[i])
         {
@@ -199,24 +193,41 @@ long SmartGuesser::firstActive()
             return minimumNumber;
         }
     }
-    
+
     return minimumNumber;
 }
 
-
-long SmartGuesser::pow(int a, int b) {
-  /// Power for integers
-  if (b == 0) {
-    return 1;
-  } else if (b == 1) {
-    return a;
-  } else {
-    long r = a;
-    for (int i = 1; i < b; i++)
-      r = r * a;
-    return r;
-  };
+//this method calculate power of integer
+long SmartGuesser::pow(int a, int b)
+{
+    /// Power for integers
+    if (b == 0)
+    {
+        return 1;
+    }
+    else if (b == 1)
+    {
+        return a;
+    }
+    else
+    {
+        long r = a;
+        for (int i = 1; i < b; i++)
+            r = r * a;
+        return r;
+    };
 }
 
+//this method complete zero's in the begining of the guess
+void SmartGuesser::complete(string guess)
+{
+    string zero = "";
+    //adding zero's
+    for (int i = guess.length(); i < p; i++)
+    {
+        zero = "0" + zero;
+    }
+    _guess = zero + _guess;
+}
 
 }; // namespace bullpgia
